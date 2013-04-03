52d1c1dc1d9eae9de2fd14251b1bd1ec5c2bd0de
Aragan123 <jeffreyzhong_2000@yahoo.com>
2013-04-03 15:26:13 +0800

    initial commit

---
+++ NancyShow/BGPageSwitcherDelegate.h
@@ -0,0 +1,32 @@
+//
+//  BGPageSwitcherDelegate.h
+//  NancyShow
+//
+//  Created by Jeff Zhong on 2013/04/03.
+//  Copyright (c) 2013 Brute Games Studio. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#ifndef kPageMain
+#define kPageMain 0
+#define kPageGalleryHome 1
+#define kPageGallery 2
+#define kPageAbout 3
+#define kPageUI 4
+#endif
+
+enum enumPage {
+	pageMain =0,
+	pageGalleryHome,
+	pageGallery,
+    pageAbout,
+    pageUI,
+};
+
+
+@protocol BGPageSwitcherDelegate <NSObject>
+
+-(void) switchViewTo: (int)toPage fromView:(int)fromPage;
+
+@end
